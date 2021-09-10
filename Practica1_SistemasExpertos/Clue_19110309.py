47c110a49c9f8545afd1946f7057fcd0c17cc10a
Jesus PG <jesuspigna98@gmail.com>
2021-09-09 22:13:46 -0500

    Agregar clase y metodos

--- Practica1_SistemasExpertos/Clue_19110309.py
+++ Practica1_SistemasExpertos/Clue_19110309.py
@@ -1,8 +1,37 @@
-
-
+##Jesús Piña García 19110309
+##Practica1 - CLUE
 import random
 
-mylist = [1, 2, 3,4, 5]
-random.shuffle(mylist)
 
-print(mylist) 
+class juego:
+    def __init__(self, personajes_iniciales, armas_iniciales, lugares_iniciales) -> None:
+        self.personajes_iniciales = personajes_iniciales
+        self.armas_iniciales = armas_iniciales
+        self.lugares_iniciales = lugares_iniciales
+
+    def randomizar_historia(self):
+        self.personajes_random = random.sample(self.personajes_iniciales, len(self.personajes_iniciales))
+        self.lugares_random = random.sample(self.armas_iniciales, len(self.lugares_iniciales))
+        self.armas_random = random.sample(self.lugares_iniciales, len(self.armas_iniciales))
+
+    def elegir_culpable(self):
+        self.culpable = self.personajes_random[0]
+        self.lugar_culpable = self.lugares_random[0]
+        self.arma_culpable = self.armas_random[0]
+
+        return self.culpable, self.lugar_culpable, self.arma_culpable ##Retornamos en orden: Culpable, lugar, arma
+    
+
+    
+
+
+
+personajes_iniciales = ["Filiponcio", "Facundo", "Giselle", "Pancracio", "Chepina"]
+armas_iniciales = ["Bolillo", "Sarten", "Cuchillo", "Hacha", "Machete"]
+lugares_iniciales = ["Almacen", "Cocina", "Herreria", "Sala", "Jardin"]
+
+
+obj_inicializacion = juego(personajes_iniciales, armas_iniciales, lugares_iniciales)
+
+obj_inicializacion.randomizar_historia()
+print(obj_inicializacion.elegir_culpable())
